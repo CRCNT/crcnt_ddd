7d3428004942688cd6fb61e6cac2fd5063da2afb
Zenas <songzenghui.cr@gmail.com>
2023-01-19 14:49:50 +0400

    feat: add `InspectErrorCode`

---
+++ ddd/src/error.rs
@@ -0,0 +1,4 @@
+/// Inspect the static error codes from an error enum.
+pub trait InspectErrorCode {
+  fn error_code(&self) -> &'static str;
+}
--- ddd/src/lib.rs
+++ ddd/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod collections;
+pub mod error;
 pub mod value;
